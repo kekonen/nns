5239aff37722773b1515daa275fd53b529c8c96b
kekonen <daniil.naumetc@gmail.com>
2018-06-27 18:55:24 +0200

    lstm

--- tfmain.py
+++ tfmain.py
@@ -7,3 +7,13 @@ from env import Guy
 g = Guy()
 g.reset()
 
+use_dropout = True
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True))
+model.add(LSTM(64, return_sequences=True))
+if use_dropout:
+    model.add(Dropout(0.5))
+model.add(TimeDistributed(Dense(vocabulary)))
+model.add(Activation('softmax'))
+
